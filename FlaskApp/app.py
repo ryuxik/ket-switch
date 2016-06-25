2b5de540f22ed5aa64e8aca0a2e71338af3ed96f
ryuxik <smunoz@mit.edu>
2016-06-25 01:51:14 -0400

    third try

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,5 +1,6 @@
 """
 This is the web app to connect to the internet and controll the light switch
+For now, to see it, type http://localhost:5000/ into browser
 """
 
 from flask import Flask, render_template
@@ -8,11 +9,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('page.html')
 
-@app.route('/hello/<name>')
-def hello(name):
-	return render_template('page.html', name = name)
+# @app.route('/hello/')
+# def hello(name):
+# 	return render_template('page.html')
 
 if __name__ == '__main__':
 	app.run(debug=True, host ='0.0.0.0')

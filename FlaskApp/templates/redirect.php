926577225f59f9018069ddce11c23652705fee94
ryuxik <smunoz@mit.edu>
2016-06-29 12:12:51 -0400

    hmm

--- FlaskApp/templates/redirect.php
+++ FlaskApp/templates/redirect.php
@@ -1,11 +1,14 @@
 <?php
-switch($_GET['mytext'])
+echo "test";
+switch($_GET['name'])
 {
     case 'Derp':
-        header('Location: http://localhost:5000/Annie');
+        header('Location: http://localhost:5000/name/Annie');
+        die();
         break;
     case 'Smol':
-        header('Location: http://localhost:5000/Santiago');
+        header('Location: http://localhost:5000/name/Santiago');
+        die();
         break;
     default:
         print "<p>Wrong code. Try again</p>";

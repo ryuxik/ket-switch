a18506262269b92fd78ca8db8abda14d4d9fd764
ryuxik <smunoz@mit.edu>
2016-06-29 00:16:01 -0400

    see documentation for explanation, for now use pin 7 as pwm

--- switchCODE.py
+++ switchCODE.py
@@ -2,18 +2,42 @@
 This is the epic project that was undertaken by two derps in June of 2016.
 :3
 """
+import RPi.GPIO as GPIO
+import time
 
-class sampleClass:
-	"""
-	Just put this here as an example. lel
-	"""
+#sets pins to be referenced by number on the r pi
+GPIO.setmode(GPIO.BOARD)
+#set pin 7 as an output pin 
+GPIO.setup(7,GPIO.OUT)
+#sets pin 7 as a pwm modulated pin
+chosenPin7 = GPIO.PWM(7, 50)
+#sets pin to set to neutral position
+def setNeutral(pin):
+	pin.start(7.5)
+#sets pin to set to 180 position
+def set180(pin):
+	pin.start(12.5)
+#sets pin to set to 0 position
+def set0(pin):
+	pin.start(2.5)
+#stops pwm 
+def stopPin(pin):
+	pin.stop()
+#demonstrates how servo is controlled by  r pi
+def trialRun(pin):
+	setNeutral(pin)
+	try:
+		while True:
+			pin.ChangeDutyCycle(7.5)
+			time.sleep(1)
+			pin.ChangeDutyCycle(12.5)
+			time.sleep(1)
+			pin.ChangeDutyCycle(2.5)
+			time.sleep(1)
 
-	def __init__(self):
-		pass
+	except KeyboardInterrupt:
+		stopPin(pin)
+		GPIO.cleanup()
 
-def main():
-	"""
-	Main method
-	"""
+trialRun(chosenPin7)
 
-if __name__ == "__main__": main()

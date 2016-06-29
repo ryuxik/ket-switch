d7ed8e9dd9bb375b658d4620d4a37f599970ac3e
ryuxik <smunoz@mit.edu>
2016-06-29 09:33:55 -0400

    minor changes to servo control

--- switchCODE.py
+++ switchCODE.py
@@ -5,39 +5,50 @@ This is the epic project that was undertaken by two derps in June of 2016.
 import RPi.GPIO as GPIO
 import time
 
-#sets pins to be referenced by number on the r pi
-GPIO.setmode(GPIO.BOARD)
-#set pin 7 as an output pin 
-GPIO.setup(7,GPIO.OUT)
-#sets pin 7 as a pwm modulated pin
-chosenPin7 = GPIO.PWM(7, 50)
+frequency = 50
+neutralDc = frequency*.15
+topDc = frequency * .25
+botDc = frequency * .05
+
+def readyBoard(pin, frequency):
+	#sets pins to be referenced by number on the r pi
+	GPIO.setmode(GPIO.BOARD)
+	#set pin as an output pin 
+	GPIO.setup(pin,GPIO.OUT)
+	#sets pin as a pwm modulated pin at a frequency
+	chosenPin = GPIO.PWM(pin, frequency)
+	#returns pin
+	return chosenPin
+
 #sets pin to set to neutral position
 def setNeutral(pin):
-	pin.start(7.5)
+	pin.start(neutralDc)
 #sets pin to set to 180 position
 def set180(pin):
-	pin.start(12.5)
+	pin.start(topDc)
 #sets pin to set to 0 position
 def set0(pin):
-	pin.start(2.5)
+	pin.start(botDc)
 #stops pwm 
 def stopPin(pin):
 	pin.stop()
 #demonstrates how servo is controlled by  r pi
-def trialRun(pin):
-	setNeutral(pin)
+def trialRun(pin, frequency):
+	p = readyBoard(pin, frequency)
+	setNeutral(p)
 	try:
 		while True:
-			pin.ChangeDutyCycle(7.5)
+			p.ChangeDutyCycle(neutralDc)
 			time.sleep(1)
-			pin.ChangeDutyCycle(12.5)
+			p.ChangeDutyCycle(topDc)
 			time.sleep(1)
-			pin.ChangeDutyCycle(2.5)
+			p.ChangeDutyCycle(botDc)
 			time.sleep(1)
 
 	except KeyboardInterrupt:
-		stopPin(pin)
+		stopPin(p)
 		GPIO.cleanup()
 
-trialRun(chosenPin7)
+#uncomments line below to see demo
+#trialRun(7, frequency)
 
